7b5123621af431f2f1ec3b1dae58c8cf8a972ec4
Aditya Sunil Kulkarni <adityakulkarni61@gmail.com>
2025-03-13 14:14:01 -0400

    changed width to not overflow

--- homesite/src/routes/+page.svelte
+++ homesite/src/routes/+page.svelte
@@ -6,12 +6,11 @@
 <svelte:head>
 	<title>Home</title>
 	<meta name="Homepage" content="the homepage of grantomstone" />
-	<!--img src={image} alt="background image" /-->
 </svelte:head>
-<div class="h-screen w-screen bg-cover bg-center content-center" style="background-image: url({image})">
+<div class="h-screen w-full bg-cover bg-center content-center" style="background-image: url({image})">
 	<h1 class="text-7xl font-bold text-center text-amber-700">Grantomstone</h1>
 	<br />
 	<h2 class="text-2xl text-center font-semibold text-amber-600">A guy who does not know what this name means</h2>
 </div>
-<div class="h-screen w-screen bg-slate-200">
+<div class="h-screen w-full bg-slate-200">
 </div>
